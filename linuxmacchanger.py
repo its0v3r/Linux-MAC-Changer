897a97512d705c2dec8bfa3c0b125c78763fad4d
Matheus Heidemann <105527225+matheusheidemann@users.noreply.github.com>
2022-07-22 20:46:17 -0300

    Update linuxmacchanger.py

--- linuxmacchanger.py
+++ linuxmacchanger.py
@@ -4,7 +4,9 @@
 #
 # Author: Matheus Heidemann
 #
-# Description: this is a simple MAC address changer created for Unix systems. The user can chose which interface he wants to change the MAC address (-i, # --interface) and then provide the desired MAC address (-m, --mac). Still, the user has the option to use a random generated MAC address provided by the # script. If the user don't pass any arguments, the script will ask for the user to input the peding information.
+# Description: this is a simple MAC address changer created for Unix systems. The user can chose which interface he wants to change the MAC address
+# (-i, # --interface) and then provide the desired MAC address (-m, --mac). Still, the user has the option to use a random generated MAC address
+# provided by the # script. If the user don't pass any arguments, the script will ask for the user to input the peding information.
 #
 # 22 July 2022
 #
